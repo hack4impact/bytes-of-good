59db108be0d29cf45f67a8b0f394580935fd6991
Holben888 <holben888@gmail.com>
2020-07-04 17:46:53 -0400

    feat: add boilerplate to trigger anims when scrolling to page section

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+/* Trigger the floating icon animations in the home section */
 const floatingIcons = document.querySelectorAll('svg.home-graphic-accent')
 
 const wait = (callback, timeout) =>
@@ -12,4 +13,27 @@ for (let [index, floatingIcon] of floatingIcons.entries()) {
   wait(() => {
     floatingIcon.classList.add('animate')
   }, index * 100 + 500)
+}
+
+/* Trigger animations when scrolling to a new section of the page */
+const observerOptions = {
+  root: null,
+  rootMargin: '-60% 0px -40% 0px', // trigger when 40% of the new section is visible on the page
+  threshold: 0,
+}
+
+const observerCallback = (events) => {
+  events.forEach((event) => {
+    if (event.isIntersecting) {
+      if (event.target.id === 'home') {
+        // TODO: change the nav bar background when scrolling to / away from the top of the page
+      }
+    }
+  })
+}
+
+let observer = new IntersectionObserver(observerCallback, observerOptions)
+
+for (let sectionId of ['home', 'about', 'team', 'contact']) {
+  observer.observe(document.getElementById(sectionId))
 }
