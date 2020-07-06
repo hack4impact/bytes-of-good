78b8c5889e6f3f322934711500f35977a35faf51
Holben888 <holben888@gmail.com>
2020-07-06 18:37:22 -0400

    feat: add team member filler with image styling

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ const path = require('path')
 const { promisify } = require('util')
 const pug = require('pug')
 const { rollup } = require('rollup')
+const team = require('./src/team')
 
 /* Configurable options */
 const liveReloadPort = 35729
@@ -20,6 +21,7 @@ const bundleHTML = async () => {
   const html = pug.renderFile(__dirname + '/src/index.pug', {
     env: process.env.MODE,
     liveReloadPort,
+    team,
   })
   await writeFile(__dirname + '/public/index.html', html)
 }
---
+++ src/team.js
@@ -0,0 +1,101 @@
+// memberOf supported types: 'hack4impact', 'impact labs'
+// link name supported types: 'LinkedIn', 'Twitter'
+
+module.exports = [
+  {
+    name: 'James Wang',
+    description: 'God of all things',
+    memberOf: 'hack4impact',
+    image: 'thejameswang.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    name: 'Ben Holmes',
+    description: 'Resident web dev sorceror',
+    memberOf: 'hack4impact',
+    image: 'benholmes.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    name: 'Aaron Mayer',
+    description: 'Impact Labs overlord',
+    memberOf: 'impact labs',
+    image: 'aaronmayer.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    name: 'James Wang',
+    description: 'God of all things',
+    memberOf: 'hack4impact',
+    image: 'thejameswang.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    name: 'Ben Holmes',
+    description: 'Resident web dev sorceror',
+    memberOf: 'hack4impact',
+    image: 'benholmes.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    name: 'Aaron Mayer',
+    description: 'Impact Labs overlord',
+    memberOf: 'impact labs',
+    image: 'aaronmayer.jpg',
+    links: [
+      {
+        name: 'LinkedIn',
+        href: '',
+      },
+      {
+        name: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+]
