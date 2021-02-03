0f63ec21ecd35ad19bb846f80bb6fbe9a3a35bcc
Holben888 <bholmesdev@gmail.com>
2021-02-02 21:50:38 -0500

    feat: pull team section from Contentful

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -1,19 +1,38 @@
 const fetchContent = require('../utils/fetch-content')
 
 module.exports = async function liveBroadcasts() {
-  const { upcomingBroadcastCollection } = await fetchContent(`{
+  const { upcomingBroadcastCollection, teamSection } = await fetchContent(`{
     upcomingBroadcastCollection: liveBroadcastCollection(where: {upcoming: true}, limit: 1) {
       items {
         rsvpLink
       }
     }
+    teamSection(id: "7FNNCibQRHyoyBH5QDvdhB") {
+      profilesCollection {
+        items {
+          name
+          jobTitle
+          image {
+            url
+          }
+          twitter
+          linkedIn
+        }
+      }  
+    }
   }`)
 
   const upcomingBroadcasts = upcomingBroadcastCollection?.items ?? []
+  const teamMembers = teamSection?.profilesCollection?.items ?? []
+
+  if (!teamMembers.length) {
+    throw 'There was a problem finding profiles for the Team section'
+  }
 
   return {
     // grab the RSVP link from the first upcoming broadcast
     // there should only be 1 upcoming at a time!
     rsvpLink: upcomingBroadcasts[0]?.rsvpLink,
+    teamMembers,
   }
 }
